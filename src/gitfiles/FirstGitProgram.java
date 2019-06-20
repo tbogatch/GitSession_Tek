f1b67d0836694ba9382260fb261a590cd7ddb2ff
Bogatchkina <tbogatch@teksystems.com>
2019-06-20 13:19:04 -0400

    commit ???

--- src/gitfiles/FirstGitProgram.java
+++ src/gitfiles/FirstGitProgram.java
@@ -3,6 +3,7 @@ package gitfiles;
 public class FirstGitProgram {
 
 	public static void main(String[] args) {
+		//syso + cntr + spacebar
 		System.out.println("Git Learning");
 
 	}
---
+++ src/gitfiles/SecondGitProgram.java
@@ -0,0 +1,10 @@
+package gitfiles;
+
+public class SecondGitProgram {
+
+	public static void main(String[] args) {
+		System.out.println("SecondGitProgram");
+
+	}
+
+}
